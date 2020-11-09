4f8001a53bd745b6a3f982bf0c38278801914e76
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-09 21:18:13 +0200

    changes

--- music-app/src/components/containers/Playlist.js
+++ music-app/src/components/containers/Playlist.js
@@ -3,13 +3,13 @@ import PlaylistHeader from '../PlaylistHeader';
 import PlaylistItems from '../containers/PlaylistItems';
 import NightMode from '../NightMode';
 import StyledPlaylist from '../styles/StyledPlaylist';
-import PlaylistItem from '../PlaylistItem';
+
 
 const Playlist = ({videos, active, nightModeCallback, nightMode}) => (
     <StyledPlaylist>
     <NightMode nightModeCallBack={nightModeCallback} nightMOde={nightMode}/>
     <PlaylistHeader active={active} total={videos.length}/>
-    <PlaylistItem video={videos} active={active}/>
+    <PlaylistItems videos={videos} active={active}/>
     </StyledPlaylist>
 )
 export default Playlist;
