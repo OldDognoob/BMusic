9fdc4c850e74ab6bbd413a85be825036e2b58977
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-09 12:06:11 +0200

    hoc-meaning

---
+++ music-app/src/components/hoc/withLink.js
@@ -0,0 +1,3 @@
+/*HOC or High Order Component: is an advance technique in React for rending component logic.
+A High Order Component : is a function that takes a component and returns a new component */
+
