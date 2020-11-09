c4ffba4f901929cb5ecc9dab57547de0d6e6800d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-09 12:29:07 +0200

    first-style-file-created

---
+++ music-app/src/components/styles/StyledWbnPlayer.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledWbnPlayer = styled.Div`
+background: ${props => props.theme.bgcolor};
+border:${props=> props.theme.border};
+max-width:1900px;
+margin: 0 auto;
+display: -webkit-box;
+display: flex;
+flex-direction: row;
+max-height: 863px;
+transition: all 0.5s ease;
+
+@media screen and (max-width: 1400px){
+    display: block;
+    max-height: 10000px;
+}
+`;
+export default StyledWbnPlayer;
