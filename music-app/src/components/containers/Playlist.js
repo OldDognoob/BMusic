5786b6987bb8232c118fd561095d7325c1273a63
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-09 20:09:40 +0200

    playlist

--- music-app/src/components/containers/Playlist.js
+++ music-app/src/components/containers/Playlist.js
@@ -3,12 +3,13 @@ import PlaylistHeader from '../PlaylistHeader';
 import PlaylistItems from '../containers/PlaylistItems';
 import NightMode from '../NightMode';
 import StyledPlaylist from '../styles/StyledPlaylist';
+import PlaylistItem from '../PlaylistItem';
 
-const Playlist = props => (
+const Playlist = ({videos, active, nightModeCallback, nightMode}) => (
     <StyledPlaylist>
-    <NightMode/>
-    <PlaylistHeader/>
-    <PlaylistItems/>
+    <NightMode nightModeCallBack={nightModeCallback} nightMOde={nightMode}/>
+    <PlaylistHeader active={active} total={videos.length}/>
+    <PlaylistItem video={videos} active={active}/>
     </StyledPlaylist>
 )
 export default Playlist;
